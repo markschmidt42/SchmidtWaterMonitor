8a412b51508cb6a56d48c1a8e07e5567509cf666
Mark Schmidt <mschmidt@bizstream.com>
2024-07-07 08:07:40 -0400

    fixed tank too high level

--- constants.h
+++ constants.h
@@ -15,5 +15,5 @@
 // our Tank typically starts filling at the 240 mark (120 mark on one of the tanks)
 #define TANK_LEVEL_TOO_LOW_VALUE 230 // give it a 10 gallon buffer before it alerts
 
-// our Tank typically starts filling at the 320 mark (180 mark on one of the tanks)
-#define TANK_LEVEL_TOO_HIGH_VALUE 330 // give it a 10 gallon buffer before it alerts
+// our Tank typically starts filling at the 360 mark (180 mark on one of the tanks)
+#define TANK_LEVEL_TOO_HIGH_VALUE 370 // give it a 10 gallon buffer before it alerts
